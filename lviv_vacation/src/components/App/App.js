277cbad96bf09310de0c4ef57c103889a60b473b
Admin <admin@bocaccio.edvantis.com>
2019-06-11 16:58:04 +0300

    initial change

--- lviv_vacation/src/components/App/App.js
+++ lviv_vacation/src/components/App/App.js
@@ -1,1 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+export default class App extends Component{
+    render() {
+        return (
+
+            <h1>Types of Food:</h1>
+
+        );
+      }
+}
