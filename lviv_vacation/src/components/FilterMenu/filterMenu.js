df7bb661a3a89e6455163ebe697b2fe299efa0bf
Admin <admin@bocaccio.edvantis.com>
2019-06-13 20:08:49 +0300

    check menu

--- lviv_vacation/src/components/FilterMenu/filterMenu.js
+++ lviv_vacation/src/components/FilterMenu/filterMenu.js
@@ -3,13 +3,39 @@ import ItemForCheck from './itemForCheck';
 import ButtonFilter from './buttonFilter';
 
 export default class FilterMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chooise: []
+    }
+  }
+ 
+
+  updateInfo = (value) => {
+    this.setState(function(state, props) {
+
+      this.state.chooise.push(value);
+
+      
+      // state.chooise.push(value);
+      
+      // state.chooise.forEach((element, index) => {
+      //   if(element.establish == value.establish) {
+      //     state.chooise[index] = value;
+      //   }
+      // });
+      // console.log(this.state);
+
+    });
+  }
+
   render() {
     let smallScreenClass = `preferences`;
 
     if(this.props.smallscreen) {
       smallScreenClass = `preferences ${this.props.smallscreen}`;
     }
-    
+
     return (
       <div className={smallScreenClass} >
         <div className="form-preferences">
@@ -18,11 +44,11 @@ export default class FilterMenu extends React.Component {
               <div className="check-item ">
                 <legend className="choose-pref">Choose your preferences:</legend>
               </div>
-              <ItemForCheck content="Choose all" />
-              <ItemForCheck content="Hotel" />
-              <ItemForCheck content="Motel" />
-              <ItemForCheck content="Hostel" />
-              <ItemForCheck content="Flat" />
+              <ItemForCheck content="Choose all" update={this.updateInfo}/>
+              <ItemForCheck content="Hotel" update={this.updateInfo}/>
+              <ItemForCheck content="Motel" update={this.updateInfo}/>
+              <ItemForCheck content="Hostel" update={this.updateInfo}/>
+              <ItemForCheck content="Flat" update={this.updateInfo}/>
             </div>
             <ButtonFilter />
           </form>
--- lviv_vacation/src/components/FilterMenu/itemForCheck.js
+++ lviv_vacation/src/components/FilterMenu/itemForCheck.js
@@ -4,20 +4,25 @@ export default class ItemForCheck extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isChecked: false
+      checked: true,
+      establish: this.props.content
     };
-    this.isChecked = this.isChecked.bind(this)
+
   }
   
   isChecked() {
-    console.log(this);
+    this.setState({
+      checked: !this.state.checked
+    });
+
+    // console.log(this.state.checked, this.state.establish);
   }
 
   render() {
     return (
       <div>
         <label className="check-item">
-        <input type="checkbox" onChange={this.isChecked} className="checkbox" id="checkbox" />
+        <input type="checkbox" onChange={() => {this.isChecked(); this.props.update(this.state)}} className="checkbox" id="checkbox" />
         <span className="styled-checkbox"></span>
         <span className="selection-point">{this.props.content}</span>
         </label>
