1bbb4ff7ab259f2dafd6215856e52da48847ebdb
Admin <admin@bream.edvantis.com>
2019-06-13 15:21:09 +0300

    fixed

--- lviv_vacation/src/components/Navbar/Navbar.js
+++ lviv_vacation/src/components/Navbar/Navbar.js
@@ -17,7 +17,7 @@ class Navbar extends Component{
                     <div className="nav-cng">
                         <li className="navbarLi">
                             <NavLink activeClassName="active" to='/about'>
-                                About
+                                About 
                             </NavLink>
                         </li>
 
