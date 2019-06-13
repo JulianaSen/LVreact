f79f55cd3f7374563630cb979ddfb436f4fdddd4
Oleksii Semko <oleksa120699@gmail.com>
2019-06-13 08:01:35 +0300

    Add navbar

---
+++ lviv_vacation/src/components/Navbar/Navbar.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import favicon from '../../img/around.png';
+import { NavLink } from 'react-router-dom';
+
+class Navbar extends Component{
+    render() {
+
+        const {links} = this.props;
+        return (
+            <div>
+                <header className="header">
+                <ul className="navbar">
+                    <li className="navbarLi"><NavLink to='/main'>
+                        <img className="logo" src={favicon} alt="logo" />
+                    </NavLink></li>
+                    <div className="nav-cng">
+                        <li className="navbarLi">
+                            <NavLink activeClassName="active" to='/about'>
+                                About
+                            </NavLink>
+                        </li>
+
+                        <li className="navbarLi">
+                            <NavLink activeClassName="active" to='/sign_in'>
+                                Sign In
+                            </NavLink>
+                        </li>
+
+                        <li className="navbarLi">
+                            <NavLink activeClassName="active" to='/sign_up'>
+                                Sign Up
+                            </NavLink>
+                        </li>
+                    </div>
+                </ul>
+            </header>
+        </div>
+        )
+    }
+}
+
+
+export default Navbar;
