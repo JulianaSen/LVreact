a8216b96e0a9ec9dad8fc43247dec0617680a4e6
Admin <admin@bream.edvantis.com>
2019-06-18 13:21:59 +0300

    sign up validation

--- lviv_vacation/src/components/SignIn/SignIn.js
+++ lviv_vacation/src/components/SignIn/SignIn.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
-import Btn from '../Forms/Btn';
-import Input from '../Forms/Input';
+//import Btn from '../Forms/Btn';
+//import Input from '../Forms/Input';
 import Navbar from "../Navbar/Navbar";
-import { tsThisType } from '@babel/types';
+//import { tsThisType } from '@babel/types';
 
 class SignIn extends Component{
     state = {
@@ -14,7 +14,15 @@ class SignIn extends Component{
     handleSubmit = event => {
         event.preventDefault();
         console.log(this.state);
-        console.log(this.state.email);
+    };
+
+    handleChange = event => {
+        const isCheckBox = event.target.type === "checkbox";
+        this.setState ({
+            [event.target.name]: isCheckBox
+            ? event.target.checked
+            : event.target.value
+        });
     };
 
     render(){
@@ -24,31 +32,25 @@ class SignIn extends Component{
                 <section className="rgt">
                     <p className = "createText">Sign in:</p>
                     <form className="rgt-form" onSubmit={this.handleSubmit}>
-                        <Input
-                            //csform="rgt-form"
-                            csdiv="input-rgt"
-                            tp="email"
-                            nm="email"
-                            pdr="email"
-                            value={this.state.email}
-                        />
-
-                        <Input
-                            //csform="rgt-form"
-                            csdiv="input-rgt"
-                            tp="password"
-                            nm="password"
-                            pdr="password"
-                            value={this.state.password}
-                        />
-
-                        <Btn
-                            //csform="rgt-form"
-                            csdiv="input-rgt"
-                            idt="btn"
-                            tp="submit"
-                            vl="Submit"
-                        />
+                        <div className="input-rgt">
+                            <input 
+                                type="email" 
+                                name="email" 
+                                placeholder="e-mail" 
+                                value={this.state.email}
+                                onChange={this.handleChange}
+                            />
+                            <br />
+                            <input 
+                                type="password" 
+                                name="password" 
+                                placeholder="password" 
+                                value={this.state.password}
+                                onChange={this.handleChange}
+                            />
+                            <br />
+                            <input id="btn" type="submit" value="Submit" />
+                        </div>
                     </form>
 
                     <p className="changeLink">Don't have an account?</p>
--- lviv_vacation/src/components/SignUp/SignUp.js
+++ lviv_vacation/src/components/SignUp/SignUp.js
@@ -1,57 +1,106 @@
 import React, {Component} from 'react';
-import Btn from '../Forms/Btn';
-import Input from '../Forms/Input';
+//import Btn from '../Forms/Btn';
+//import Input from '../Forms/Input';
 import Navbar from  "../Navbar/Navbar";
 
-class SignUp extends Component{
-    render(){
+const initialState = {
+    userName: "",
+    email: "",
+    password: "",
+    confirm: "", 
+    error: ""
+}
 
+class SignUp extends Component{
+    state = initialState;
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const isValid = this.validate();
+        if(isValid) {
+            console.log(this.state);
+            this.setState(initialState);
+        }
+    }
+
+    handleChange = event => {
+        const isCheckBox = event.target.type === "checkbox";
+        this.setState ({
+            [event.target.name]: isCheckBox
+            ? event.target.checked
+            : event.target.value
+        });
+    }
+
+    validate = () => {
+        let error = "";
+        if(this.state.password != this.state.confirm) {
+            error = "Passwords aren't equal!";
+        }
+
+        if(!this.state.userName 
+            || !this.state.email 
+            || !this.state.password 
+            || !this.state.confirm) {
+            error = "Fields can't be empty!";
+        }
+
+        if(error) {
+            this.setState({ error });
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+    render(){
         return (
             <div className="rst">
                 <Navbar />
                 <section className="rgt">
-                <p className = "createText">Create new account:</p>
-                <form className="rgt-form">
-                        <Input
-                             //csform="rgt-form"
-                             csdiv="input-rgt"
-                             tp="text"
-                             nm="username"
-                             pdr="username"
-                        />
-
-                        <Input
-                             //csform="rgt-form"
-                             csdiv="input-rgt"
-                             tp="email"
-                             nm="email"
-                             pdr="email"
-                        />
-
-                        <Input
-                             //csform="rgt-form"
-                             csdiv="input-rgt"
-                             tp="password"
-                             nm="password"
-                             pdr="password"
-                        />
-
-                        <Input
-                             //csform="rgt-form"
-                             csdiv="input-rgt"
-                             tp="password"
-                             nm="password"
-                             pdr="confirm password"
-                        />
-
-                        <Btn
-                            // csform="rgt-form"
-                             csdiv="input-rgt"
-                             idt="btn"
-                             tp="submit"
-                             vl="Register"
-                        />
-</form>
+                    <p className = "createText">Create new account:</p>
+                    <form className="rgt-form" onSubmit={this.handleSubmit}>
+                        <div className="input-rgt">
+                            <input 
+                                type="text" 
+                                name="userName" 
+                                placeholder="username" 
+                                value={this.state.userName}
+                                onChange={this.handleChange}
+                            />
+                            <br />
+                            <input 
+                                type="email" 
+                                name="email" 
+                                placeholder="email" 
+                                value={this.state.email}
+                                onChange={this.handleChange}
+                            />
+                            <br />
+                            <input 
+                                type="password" 
+                                name="password" 
+                                placeholder="password" 
+                                value={this.state.password}
+                                onChange={this.handleChange}
+                            />
+                            <br />
+                            <input 
+                                type="password" 
+                                name="confirm" 
+                                placeholder="repeat password" 
+                                value={this.state.confirm}
+                                onChange={this.handleChange}
+                            />
+                            <br />
+                            <div style={{fontSize: "30px", backgroundColor: "white", borderRadius: "5px", marginTop: "5px"}}>
+                                {this.state.error}
+                            </div>
+                            <input id ="btn" type="submit" value="Register" />
+                        </div>
+                    </form>
                 </section>
             </div>
         )
