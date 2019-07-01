ce6b60218f6a283b6a2d3c25faf32a52b41a2dfc
Admin <admin@bullhead.edvantis.com>
2019-07-01 21:50:01 +0300

    Add basket icon

--- lviv_vacation/src/components/CatalogWithFilter/CatalogWithFilter.js
+++ lviv_vacation/src/components/CatalogWithFilter/CatalogWithFilter.js
@@ -2,59 +2,99 @@ import React from 'react';
 import FilterMenu from '../FilterMenu/filterMenu';
 import Catalog from '../Catalog/Catalog';
 // import { store } from '../../actions/actionFilterMenu';
-import { WrapperComponent } from '../FilterMenu/filterMenu'
-import {store} from "../../index";
-import BurgerContainer from './BurgerContainer'
+import { WrapperComponent } from '../FilterMenu/filterMenu';
+import BurgerContainer from './BurgerContainer';
+import basket from "../../img/basket.png";
+import { connect } from 'react-redux';
+import Basket from "../Catalog/modal";
 
+class CatalogWithFilter extends React.Component {
 
-export default class CatalogWithFilter extends React.Component {
+    constructor () {
+      super();
 
-  state = {
-    drawerOpen: false
-  }
-
-  drawerToggleClickHandler = () => {
-    this.setState(function(prevState) {
-      return {drawerOpen: !prevState.drawerOpen};
-    })
-  };
-
-
-  render() {
-
-    let showFilterMenu = null;
-    let classNameFilterMenu = "delete-margin";
-    let navContainerClass = "nav-cont-without-filter-menu";
-    if(this.state.drawerOpen) {
-      //showFilterMenu = <Provider store={store}><WrapperComponent smallscreen="small-screen"/></Provider>
-      classNameFilterMenu = "main-container";
-      navContainerClass = "nav-container";
+      //state for show and hide modal window
+      this.state = {
+        showModal: false
+      };
+      
+      //bind function for changing modal state
+      this.handleOpenModal = this.handleOpenModal.bind(this);
+      this.handleCloseModal = this.handleCloseModal.bind(this);
     }
-    
-    return (
-      <div className="mainContent">
-        <div className="nav-container">
-          <label className="burger-cont">
-            <span></span>
-            <input type="checkbox" className="hamburger" />
-            <div className="nav-icon">
-              <div></div>
-            </div>
-          </label>
-            <WrapperComponent smallscreen="small-screen"/>
-        </div>
-        <div className={navContainerClass}>
-          <BurgerContainer clickBurger={this.drawerToggleClickHandler}/>;
-          {showFilterMenu}
-        </div>
-          
-        <main className={classNameFilterMenu}>
-          <div className="wave-block">
-           <WrapperComponent />
-            <Catalog />
+
+    handleOpenModal () {
+      this.setState({ showModal: true });
+    }
+
+    handleCloseModal () {
+      this.setState({ showModal: false });
+    }
+
+    state = {
+      drawerOpen: false
+    }
+
+    drawerToggleClickHandler = () => {
+      this.setState(function(prevState) {
+        return {drawerOpen: !prevState.drawerOpen};
+      })
+    };
+
+
+    render() {
+
+      let showFilterMenu = null;
+      let classNameFilterMenu = "delete-margin";
+      let navContainerClass = "nav-cont-without-filter-menu";
+      if(this.state.drawerOpen) {
+        classNameFilterMenu = "main-container";
+        navContainerClass = "nav-container";
+      }
+      
+      return (
+        <div className="mainContent">
+          <div className="nav-container">
+            <label className="burger-cont">
+              <span></span>
+              <input type="checkbox" className="hamburger" />
+              <div className="nav-icon">
+                <div></div>
+              </div>
+            </label>
+              <WrapperComponent smallscreen="small-screen"/>
           </div>
-        </main>
-      </div>
-    )
+          <div className={navContainerClass}>
+            <BurgerContainer clickBurger={this.drawerToggleClickHandler}/>;
+            {showFilterMenu}
+          </div>
+            
+          <main className={classNameFilterMenu}>
+            <div className="wave-block">
+            <WrapperComponent />
+              <Catalog />
+              
+              {/* Basket button */}
+                  <div className="align-basket">
+                  <div className="basket" onClick={this.handleOpenModal}>
+                    <img src={basket}/>
+                    <div className="counter"><p>{this.props.userItems.length}</p></div>
+                  </div>
+              </div>
+
+            </div>
+          </main>
+
+
+            <Basket showModal={this.state.showModal} handleCloseModal={this.handleCloseModal}/>
+        </div>
+      )
+    }
   }
-}
+
+
+const mapStateToProps = state => ({
+  userItems: state.data.userItems,
+});
+
+export default connect(mapStateToProps)(CatalogWithFilter);
