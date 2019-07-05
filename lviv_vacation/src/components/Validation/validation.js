a58f86d0591c0a871448b719d5abd585ecf9d0cf
Admin <admin@bream.edvantis.com>
2019-07-05 11:21:45 +0300

    redux forms

--- lviv_vacation/src/components/Validation/validation.js
+++ lviv_vacation/src/components/Validation/validation.js
@@ -1,27 +1,5 @@
 const validate = values => {
     const errors = {}
-  
-    if (!values.email) {
-      errors.email = "Email can't be blank!";
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address'
-    }
-  
-    if (!values.password) {
-      errors.password = "Password can't be blank!";
-    } 
-
-    if (!values.userName) {
-        errors.userName = "Name can't be blank!";
-    } 
-
-    if (!values.confirm) {
-        errors.confirm = "Password can't be blank!";
-    } 
-
-    if(values.password !== values.confirm) {
-        errors.confirm = "Passwords must be equal!";
-    }
 
     if(!values.budget) {
         errors.budget = "Budget can't be blank!";
